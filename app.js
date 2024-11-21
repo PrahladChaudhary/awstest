0b1644f0c39de1223f655b084214b624909d6d3d
prahlad.singh <prahlad.singh@canstar.com.au>
2024-11-21 18:09:16 +0530

    all ip

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is listening on port ${PORT} from any IP address.`);
 });
